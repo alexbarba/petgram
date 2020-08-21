366ef82655a423807d7f2eab6cf17d7605623fb3
Alexis Barba <alexbarba.2278@gmail.com>
2020-08-20 21:15:00 -0500

    Creando ListOfCategories y estilos globales

---
+++ src/components/Category/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Anchor = styled.a`
+display: flex;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    width:75px;
+    text-decoration: none;
+    
+`
+
+export const Image = styled.img`
+    border:1px solid #ddd;
+    box-shadow: 0px 10px 14px rgba(0, 0, 0, 0.2);
+    border-radius: 50%;
+    height:75px;
+    width:75px;
+    overflow: hidden;
+    object-fit: cover;
+`
