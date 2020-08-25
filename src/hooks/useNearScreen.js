d7481e8375f1d524bff1bf148343f4075cef2527
Alexis Barba <alexbarba.2278@gmail.com>
2020-08-25 14:41:15 -0500

    Custom Hooks: useNearScreen y useLocalStorage. Volviendo los hooks reutilizables.

---
+++ src/hooks/useNearScreen.js
@@ -0,0 +1,26 @@
+import { useEffect, useState, useRef } from 'react'
+
+export function useNearScreen () {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    // Si el navegador no soporta intersection-observer, importamos la libreria, si lo soporta solo regresamos el intersection observer
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(element.current)
+    })
+  }, [element])
+
+  return [show, element]
+}
