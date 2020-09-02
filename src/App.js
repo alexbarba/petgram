7855c4e9e54b79203da980ff018028cdf874a8f4
Alexis Barba <alexbarba.2278@gmail.com>
2020-09-01 22:54:37 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import { Logo } from './components/Logo'
 import { Detail } from './pages/Detail'
 import { Home } from './pages/Home'
 import { NavBar } from './components/NavBar'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 export const App = () => {
   return (
@@ -17,6 +24,21 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='detail/:detailId' />
       </Router>
+
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+                </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+                </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
