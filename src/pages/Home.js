2e5712cfb11329cb1145b8d5e09ab3e0e4a9eb57
Alexis Barba <alexbarba.2278@gmail.com>
2020-09-07 13:02:01 -0500

    Retoques a las rutas de la app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,10 @@ import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 
 export const Home = (path) => {
-  console.log('path >', path)
   return (
     <>
       <ListOfCategories />
-      <ListOfPhotoCards categoryId={path.id} />
+      <ListOfPhotoCards categoryId={path.categoryId} />
     </>
   )
 }
